12ba17522646c1ffa470928596b4f928052deeb3
Daley Meistrell <dstrell14@gmail.com>
2015-06-06 12:46:01 -0700

    push to heroku

--- Gemfile
+++ Gemfile
@@ -16,6 +16,12 @@ gem 'coffee-rails', '~> 4.1.0'
 
 gem 'unicorn'
 
+# Gemfile
+gem 'figaro'
+
+#lets us use paperclip with S3 easily
+gem 'aws-sdk', '< 2.0'
+
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,13 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => 'sunglasses-project'
+  }
+}
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
